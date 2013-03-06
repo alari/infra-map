df07a69472186e792a9cc939d9d9b24c1b20e73e
Igor Korsunov <elquendi@yandex.ru>
2013-03-06 22:11:32 +0400

    Add initial plugin info

--- InfraMapGrailsPlugin.groovy
+++ InfraMapGrailsPlugin.groovy
@@ -10,19 +10,19 @@ class InfraMapGrailsPlugin {
 
     // TODO Fill in these fields
     def title = "Infra Map Plugin" // Headline display name of the plugin
-    def author = "Your name"
+    def author = "Igor Korsunov"
     def authorEmail = ""
     def description = '''\
-Brief summary/description of the plugin.
+Customizable map directive for AngularJS.
 '''
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/infra-map"
+    def documentation = ""
 
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
 //    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
@@ -34,7 +34,7 @@ Brief summary/description of the plugin.
 //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/alari/infra-map" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
