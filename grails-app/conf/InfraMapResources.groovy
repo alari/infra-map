f33c381d54417b66b8de1b181e7edf5262a0fe3c
Igor Korsunov <elquendi@yandex.ru>
2013-03-07 00:33:44 +0400

    Add Leaflet to the resources

---
+++ grails-app/conf/InfraMapResources.groovy
@@ -0,0 +1,7 @@
+modules = {
+    leaflet {
+        resource "http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.css"
+        resource "http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.ie.css"
+        resource "http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.js"
+    }
+}
