c08569aad1f6cd4886ee35dba60583c4e9be9f90
Igor Korsunov <elquendi@yandex.ru>
2013-03-09 22:00:49 +0400

    Add CoffeeScript files to the resources

--- grails-app/conf/InfraMapResources.groovy
+++ grails-app/conf/InfraMapResources.groovy
@@ -1,4 +1,15 @@
 modules = {
+    'infra-map' {
+        resource url: [dir: "coffee", file: "factory.coffee", plugin: "infra-map"]
+        resource url: [dir: "coffee", file: "behaviours.coffee", plugin: "infra-map"]
+        resource url: [dir: "coffee", file: "directives.coffee", plugin: "infra-map"]
+        dependsOn 'angular', 'leaflet', 'infra-map-utils'
+   }
+
+    'infra-map-utils' {
+        resource url: [dir: "coffee", file: "utils.coffee", plugin: "infra-map"]
+    }
+
     leaflet {
         resource "http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.css"
         resource "http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.ie.css"
